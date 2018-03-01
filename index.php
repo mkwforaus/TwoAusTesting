11190560d00be749c9d227077644becbb88e0f8f
mkwforaus <35763121+mkwforaus@users.noreply.github.com>
2018-03-01 13:44:09 +1000

    Update and rename index.html to index.php
    PHP

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Sample Deployment</title>
+</head>
+<body>
+  <div align="center">
+    <h1>Congratulations!</h1>
+  </div>
+</body>
+</html>
+
